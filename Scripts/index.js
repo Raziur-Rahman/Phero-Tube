64bb928325c410b30c4572273a78ec150c642e1e
Raziur-Rahman <mdraziurrahman051@gmail.com>
2023-09-02 17:27:03 +0600

    Sort by valu and empty html code added

--- Scripts/index.js
+++ Scripts/index.js
@@ -1,10 +1,11 @@
 
-const handleCategories = async () => {
+const CategoriesTab = async () => {
     const categoriesContainer = document.getElementById("Categories-Container");
 
     const response = await fetch(`https://openapi.programming-hero.com/api/videos/categories`);
     const dataObject = await response.json();
     const data = dataObject.data;
+    
     data.forEach(element => {
         const tab = document.createElement('div');
         tab.classList = `tabs mx-1 my-2 md:mx-3 tabs-boxed bg-gray-300 hover:bg-amber-500`
@@ -13,9 +14,8 @@ const handleCategories = async () => {
             `;
         categoriesContainer.appendChild(tab);
     });
-    handleCategory('1000');
-    // console.log(data.data);
-    // console.log(data);
+    
+    handleCategory(`${data[0].category_id}`);
 }
 
 const handleCategory = async (ctgId) => {
@@ -23,49 +23,53 @@ const handleCategory = async (ctgId) => {
     const response = await fetch(`https://openapi.programming-hero.com/api/videos/category/${ctgId}`);
 
     const dataObject = await response.json();
-    console.log(dataObject.status);
     console.log(dataObject);
     const data = dataObject.data;
-    if (dataObject.status === true) {
-        categoriesDataLoad(data);
-        btn.addEventListener('click', function () {
-            sortByView(ctgId);
-            return;
-        });
-    }
-    else{
-        categoriesDataLoad(data);
+    categoriesDataLoad(data);
+    const Sorted = await sortByView(ctgId);
+
+    // sort by value hanlde here
+    btn.addEventListener('click', function () {
+        if(Sorted === ''){
+            notFound();
+        }
+        else{
+            categoriesDataLoad(Sorted);
+        }
+    });
+
+    if (dataObject.status !== true) {
         notFound();
-        console.log("data not found");
     }
 
 }
-const notFound = () =>{
+
+const notFound = () => {
     const cardContainer = document.getElementById('card-container');
     cardContainer.innerHTML = '';
     // const notFoundField = document.getElementById("content-container");
-    cardContainer.classList =`flex flex-col justify-center items-center mt-36`;
-    cardContainer.innerHTML =`
+    cardContainer.classList = `flex flex-col justify-center items-center mt-24 md:mt-28 lg:mt-36`;
+    cardContainer.innerHTML = `
         <img src="images/Icon.png" alt="Author Image...">
         <h1 class="text-3xl text-center font-bold mt-5 ">Oops!! Sorry, There is no <br>content here</h1>
     
-    `
-
+    `;
 }
 
 const categoriesDataLoad = async (data) => {
     const cardContainer = document.getElementById('card-container');
-    cardContainer.classList =`grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-5 my-10 px-5 md:px-16`;
+    cardContainer.classList = `grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-5 my-10 px-5 md:px-16`;
     cardContainer.innerHTML = '';
-    // const notFoundField = document.getElementById("content-container");
-    // notFoundField.innerText='';
 
     data.forEach(element => {
         const card = document.createElement("div");
         card.classList = `card bg-base-100 `;
 
         card.innerHTML = `
+        <div>
         <figure><img class="h-[220px] w-full rounded-2xl" src="${element?.thumbnail}" alt="image" /></figure>
+        <p class="absolute top-[190px] right-[7px] text-white bg-[#171717] rounded-lg px-1 ">${ConvertPostedDate(element.others.posted_date)}</p>
+        </div>
         <div class="card-body px-0 flex flex-row">
             <img class="rounded-full h-16 w-16  p-0" src="${element?.authors[0]?.profile_picture}" alt="Author Image...">
             <div>
@@ -80,17 +84,47 @@ const categoriesDataLoad = async (data) => {
         </div>
         `;
         cardContainer.appendChild(card);
-    })
-    // console.log(data);
+    });
 
+}
+
+const ConvertPostedDate =(date) =>{
+    if(date !== ''){
+        let timeInSec = parseFloat(date); 
+        let timeInMin = Math.floor(timeInSec/60);
+        let timeInHour = timeInMin/60;
+        if(timeInHour >= 24 ){
+            let timeInDay = timeInHour/24;
+            const timeInYear = (timeInDay/365).toString();
+            const [x, y] = [a,b] = [...timeInYear.split('.')];
+            timeInDay = Math.floor(parseFloat(`.${y}`)*365);
+            return `${x}years ${timeInDay}days ago` ;
+        }
+        else if(timeInHour < 24) {
+            const temp = timeInHour.toString();
+            const [a,b] = [...temp.split('.')];
+            timeInMin = Math.floor(parseFloat(`.${b}`)*60);
+
+            return `${a}hrs ${timeInMin} min ago`;
+        }
+        
+    }
+    else{
+        return '';
+    } 
 }
 
 const sortByView = async (ctgId) => {
     const response = await fetch(`https://openapi.programming-hero.com/api/videos/category/${ctgId}`);
 
     const dataObject = await response.json();
-    const data =await dataObject.data;
-    data.sort(function (a, b) {
+    const data = dataObject.data;
+
+    if(dataObject.status === false){
+        return '';
+    }
+
+    const sortedData = data.sort(function (a, b) {
         if (parseFloat(a.others.views.slice(0, a.others.views.length - 1)) < parseFloat(b.others.views.slice(0, b.others.views.length - 1))) {
             return -1;
         }
@@ -99,8 +133,10 @@ const sortByView = async (ctgId) => {
         }
         return 0;
 
-    })
-    let datta = await data.reverse();
-    categoriesDataLoad(datta);
+    });
+
+    let datta = sortedData.reverse();
+    return datta;
 }
-handleCategories();
+
+CategoriesTab();
